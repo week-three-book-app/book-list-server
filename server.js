bf4e926ff569e63273223045eb2d9d91ccc7e1c9
Josiah Green <JosiahGreen90@gmail.com>
2018-02-27 15:11:29 -0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// application dependencies
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
@@ -12,7 +13,9 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 app.use(cors());
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+app.get('/api/v1/books', (req, res) => res.send('Testing 1, 2, 3'));
+
+// Make query here
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
