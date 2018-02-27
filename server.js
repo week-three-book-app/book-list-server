2f131bb42323f2ecf50f62a17a236b30250e7e68
Josiah Green <JosiahGreen90@gmail.com>
2018-02-26 16:39:04 -0800

    setup

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listenerCount(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // PORT=3000;
 // CLIENT_URL=http://localhost:8080;
 
-// DATABASE_URL=postrgres://localhost:5432/task_app
+// DATABASE_URL=postrgres://localhost:5432/booklist
